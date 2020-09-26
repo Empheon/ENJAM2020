64da3cc8edae711735985ff0d671744400d61040
Thomas Foucault <t.foucault2@gmail.com>
2020-09-26 10:01:34 +0200

    Add folders & audio/music managers

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static YorfLib.SingletonHelper;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitSingleton(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
