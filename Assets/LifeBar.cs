58a8ff556feb6e021b2253ce5164500c6c081a19
Pamphile <pamphile.saltel@wanadoo.fr>
2020-09-27 18:20:57 +0200

    endscreen Animation + Life Display (Please stay in 16/9 T-T)

---
+++ Assets/LifeBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+    public float Life = 100;
+
+    public GameObject Fuller;
+    public UnityEngine.UI.Image Filling;
+
+    private float LifeMax = 100;
+    private float LifeMin = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Life = LifeMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Life >= LifeMax && !Fuller.activeInHierarchy)
+        {
+            Fuller.SetActive(true);
+        }
+        else if (Life < LifeMax && Fuller.activeInHierarchy)
+        {
+            Fuller.SetActive(false);
+        }
+
+        Filling.fillAmount = Life / (LifeMax - LifeMin);
+    }
+}
