3554936ef8cd5f4c76b41b69b321b4f3a4db62db
Valentin <valentin-didomenico@orange.fr>
2020-09-27 16:00:50 +0200

    [ADD] Wwise managers and beat detection events + Scene Test

---
+++ Assets/Scripts/Audio/WwiseMusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseMusicManager : MonoBehaviour
+{
+    public AK.Wwise.Event Play_MainMusic;
+
+    public static EventHandler OnMusicBeat;
+    public static EventHandler OnMusicCue;
+
+    public static WwiseMusicManager instance;
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(gameObject);
+        else
+            instance = null;
+    }
+
+    private void Start()
+    {
+        Play_MainMusic.Post(gameObject, (uint)AkCallbackType.AK_MusicSyncAll, CallbackFunction);
+        AkSoundEngine.SetState("STATES_MainMusic", "MainMenu");
+    }
+
+    private void CallbackFunction(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        if (in_type == AkCallbackType.AK_MusicSyncBeat)
+            OnMusicBeat?.Invoke(this, null);
+        else if (in_type == AkCallbackType.AK_MusicSyncUserCue)
+            OnMusicCue?.Invoke(this, null);
+    }
+}
