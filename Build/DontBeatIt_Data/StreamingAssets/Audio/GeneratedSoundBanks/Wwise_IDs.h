392f5bbba5cabd0eda8431a4e734c7f2ca5378a3
Thomas Foucault <t.foucault2@gmail.com>
2020-09-28 01:15:24 +0200

    re-add sound bank

---
+++ Build/DontBeatIt_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BUTTHOLE_EASTER_EGG = 1871070022U;
+        static const AkUniqueID PLAY_DOG_HAMMERED = 3850567630U;
+        static const AkUniqueID PLAY_DOG_PETTED_001 = 3274882933U;
+        static const AkUniqueID PLAY_DOG_PETTED_002 = 3274882934U;
+        static const AkUniqueID PLAY_GAME_OVER = 725000905U;
+        static const AkUniqueID PLAY_LANCEMENT_PARTIE = 3459919099U;
+        static const AkUniqueID PLAY_MAINMUSIC = 1346240278U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_RATEES_001 = 1365786089U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_RATEES_002 = 1365786090U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_RATEES_003 = 1365786091U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_RATEES_004 = 1365786092U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_RATEES_005 = 1365786093U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_REUSSIES_001 = 378238028U;
+        static const AkUniqueID PLAY_PLUSIEURS_INPUTS_REUSSIES_002 = 378238031U;
+        static const AkUniqueID PLAY_UNE_INPUT_RATEE_001 = 1804409875U;
+        static const AkUniqueID PLAY_UNE_INPUT_RATEE_002 = 1804409872U;
+        static const AkUniqueID PLAY_UNE_INPUT_RATEE_003 = 1804409873U;
+        static const AkUniqueID PLAY_UNE_INPUT_RATEE_004 = 1804409878U;
+        static const AkUniqueID PLAY_UNE_INPUT_RATEE_005 = 1804409879U;
+        static const AkUniqueID PLAY_UNE_INPUT_REUSSIE_001 = 3489750788U;
+        static const AkUniqueID PLAY_UNE_INPUT_REUSSIE_002 = 3489750791U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATES_MAINMUSIC
+        {
+            static const AkUniqueID GROUP = 3351207106U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COREGAME_110BPM = 181369000U;
+                static const AkUniqueID COREGAME_120BPM = 3507987323U;
+                static const AkUniqueID COREGAME_130BPM = 74257514U;
+                static const AkUniqueID ENDING = 3966194980U;
+                static const AkUniqueID MAINMENU = 3604647259U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRANSITION_110_TO_120 = 3290135791U;
+                static const AkUniqueID TRANSITION_120_TO_130 = 4031559303U;
+            } // namespace STATE
+        } // namespace STATES_MAINMUSIC
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB_SNB = 3769741689U;
+        static const AkUniqueID EFFECTS_SNB = 2693342603U;
+        static const AkUniqueID FOLEYS_SNB = 1153587219U;
+        static const AkUniqueID MUSIC_SNB = 2767260180U;
+        static const AkUniqueID UI_SNB = 3697701945U;
+        static const AkUniqueID VOICES_SNB = 3104731246U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
